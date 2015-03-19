96c6fdbb85964e3b4ececbc3469f1d15a12f365a
Ynte <y.w.tijsma@student.rug.nl>
2015-03-19 19:49:48 +0100

    Implemented initial GUI

--- battleship.py
+++ battleship.py
@@ -1,10 +1,24 @@
 import sys
-from PyQt4 import Qt
+from PyQt4 import QtGui
 
-class Battleship(QWidget):
+class Battleship(QtGui.QWidget):
+
+	ROWS = 10
+	COLUMNS = 10
 
 	def __init__(self):
 		super(Battleship, self).__init__()
+		self.initUI()
+
+	def initUI(self):
+		grid = QtGui.QGridLayout()
+		self.setLayout(grid)
+
+		userGrid = QtGui.QTableWidget(self.ROWS, self.COLUMNS, self)
+		enemyGrid = QtGui.QTableWidget(self.ROWS, self.COLUMNS, self)
+		grid.addWidget(userGrid, 1, 1)
+		grid.addWidget(enemyGrid, 1, 2)
+		self.show()
 
 def main():
 	app = QtGui.QApplication(sys.argv)
